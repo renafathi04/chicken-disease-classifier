06b7cf727f4fa258fd9cab303448402a05fb869c
Rena <renafathima452002@gmail.com>
2024-10-20 13:41:51 +0400

    trained

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from chickendiseaseClassifier import logger
 from chickendiseaseClassifier.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 from chickendiseaseClassifier.pipeline.stage_02_prepare_base_model import PrepareBaseModelTrainingPipeline
+from chickendiseaseClassifier.pipeline.stage_03_training import ModelTrainingPipeline
 
 STAGE_NAME="Data Ingestion stage"
 try:
@@ -20,5 +21,16 @@ try:
         obj.main()
         logger.info(f">>>>>>> stage {STAGE_NAME} completed <<<<<<<<")
 except Exception as e:
-    logger.exception(e)
-    raise e 
+       logger.exception(e)
+       raise e 
+
+STAGE_NAME = "Training"
+try:
+        logger.info(f"*******************")
+        logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+        obj = ModelTrainingPipeline()
+        obj.main()
+        logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
